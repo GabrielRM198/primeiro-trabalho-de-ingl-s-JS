ee94c8c0a16274eba2f57a368c40dba64f05d84d
GabrielRM198 <gabrielrochamagalhaes0@gmail.com>
2022-07-06 21:37:28 -0300

    primeiro trabalho de ingles JS

---
+++ script.js
@@ -0,0 +1,17 @@
+//Crie 3 variáveis, cada uma com um array e dentro de cada array, insira 3 (TRÊS) Objetos, contendo: 
+//title, director e year.
+let comedy = [
+    {GrownUps},
+    {DennisDugan},
+    {Year2010}
+]
+let action = [
+    {Extraction},
+    {SamHargrave},
+    {Year2020}
+]
+let fantasy = [
+    {HarryPotterandtheGobletfFire},
+    {MikeNewell},
+    {Year2005}
+]
